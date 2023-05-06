05e65f36feb44d1f0b3f261a7a12f9520343e6e4
Tefgoo <tefgoov@gmail.com>
2023-05-05 21:25:42 -0500

    Character Animations
    Started Importing and working on Character animations, also made walking slower

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float moveSpeed = 6f;
     private bool isWalking;
     private void Update()
     {
