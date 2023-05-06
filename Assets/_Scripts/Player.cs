ae295982bc9ad892a6b2cc7ed3b254eb8c81c2e1
Tefgoo <tefgoov@gmail.com>
2023-05-06 15:34:45 -0500

    Character Animations Update
    Player can now Crouch, walk crouch, hit, walk run and idle. Pizza Boxes now go on the back of the character. Character new speed depends on state. New Machines material. New floor plane w/0 drawings.

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -6,11 +6,15 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 6f;
     private bool isWalking;
+    private bool isRunning;
+    private bool isAttacking;
+    private bool isCrouching;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
 
-       if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             inputVector.y = +1;
         }
@@ -26,7 +30,15 @@ public class Player : MonoBehaviour
         {
             inputVector.x = +1;
         }
-        inputVector = inputVector.normalized;
+
+        if (!isAttacking)
+        {
+            inputVector = inputVector.normalized;
+        }
+        else
+        {
+            inputVector = Vector2.zero;
+        }
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
@@ -35,10 +47,43 @@ public class Player : MonoBehaviour
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp( transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        isCrouching = Input.GetKey(KeyCode.C);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isAttacking = true;
+            // Trigger attack animation here
+            Debug.Log("Attack animation started");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isAttacking = false;
+            // Stop attack animation here
+            Debug.Log("Attack animation stopped");
+        }
     }
+
     public bool IsWalking()
     {
         return isWalking;
+    }
+
+    public bool IsRunning()
+    {
+        return isRunning;
+    }
+
+    public bool IsAttacking()
+    {
+        return isAttacking;
+    }
+
+    public bool IsCrouching()
+    {
+        return isCrouching;
     }
 }
--- Assets/_Scripts/PlayerAnimator.cs
+++ Assets/_Scripts/PlayerAnimator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private const string IS_WALKING = "IsWalking";
+    private const string IS_RUNNING = "IsRunning";
+    private const string IS_CROUCHING = "IsCrouching";
+    private const string IS_ATTACKING = "IsAttacking";
     [SerializeField] private Player player;
     private Animator animator;
 
@@ -16,6 +19,8 @@ public class PlayerAnimator : MonoBehaviour
     private void Update()
     {
         animator.SetBool(IS_WALKING, player.IsWalking());
+        animator.SetBool(IS_RUNNING, player.IsRunning());
+        animator.SetBool(IS_CROUCHING, player.IsCrouching());
+        animator.SetBool(IS_ATTACKING, player.IsAttacking());
     }
-
 }
